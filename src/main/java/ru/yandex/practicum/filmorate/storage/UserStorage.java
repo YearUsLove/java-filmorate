84fd90c49346a610e042c52d32a0a40922979a39
Yaroslav <ypetrunin@nota.tech>
2026-08-30 14:34:28 +0300

    add empty lines between methods in UserStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 public interface UserStorage {
     List<User> getAll();
+
     User getById(Long id);
+
     User create(User user);
+
     User update(User user);
+
     void delete(Long id);
 }
