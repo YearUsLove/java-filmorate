b23f7442edce27a98cc5deb95a2d75147b003503
Yaroslav <ypetrunin@nota.tech>
2026-08-30 14:32:23 +0300

    add empty lines between methods in FilmStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 public interface FilmStorage {
     List<Film> getAll();
+
     Film getById(Long id);
+
     Film create(Film film);
+
     Film update(Film film);
+
     void delete(Long id);
 }
